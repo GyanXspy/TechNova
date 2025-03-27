9809301e6770daba4c17f8ab46349f25d0de8ac4
GyanXspy <gyanaranjanpatra780@gmail.com>
2025-03-27 18:31:46 +0530

    gyana

---
+++ Gyana work/Frontend/script.js
@@ -0,0 +1,13 @@
+let lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", () => {
+    let navbar = document.getElementById("navbar");
+    if (window.scrollY > lastScrollY) {
+        // Scrolling down, hide navbar
+        navbar.classList.add("hidden");
+    } else {
+        // Scrolling up, show navbar
+        navbar.classList.remove("hidden");
+    }
+    lastScrollY = window.scrollY;
+});
