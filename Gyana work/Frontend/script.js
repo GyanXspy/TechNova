795a20b1c9a96829b7690be5d6c9049efd7a8fd4
GyanXspy <gyanaranjanpatra780@gmail.com>
2025-03-30 01:17:24 +0530

    me

--- Gyana work/Frontend/script.js
+++ Gyana work/Frontend/script.js
@@ -7,3 +7,10 @@ function closeSidebar(){
     const close = document.querySelector('.sideNav');
     close.style.display = 'none'
 }
+let index = 0;
+function moveSlide(step) {
+    const slides = document.querySelector('.carousel-images');
+    const totalSlides = slides.children.length;
+    index = (index + step + totalSlides) % totalSlides;
+    slides.style.transform = `translateX(-${index * 100}%)`;
+}
