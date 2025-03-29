42a9c6019b91f1c9513aa3ec42015c5b1413fb5e
GyanXspy <gyanaranjanpatra780@gmail.com>
2025-03-30 00:39:12 +0530

    me

---
+++ Gyana work/Frontend/script.js
@@ -0,0 +1,9 @@
+function showSidebar(){
+    const sidebar = document.querySelector('.sideNav');
+    sidebar.style.display = 'flex';
+
+}
+function closeSidebar(){
+    const close = document.querySelector('.sideNav');
+    close.style.display = 'none'
+}
