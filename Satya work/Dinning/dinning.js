cedd2e6d56b532149f927268e08974e6e2a3f274
Satyabrata-123 <malliksatyabrata29@gmail.com>
2025-04-06 17:06:08 +0530

    hi

--- Satya work/Dinning/dinning.js
+++ Satya work/Dinning/dinning.js
@@ -6,7 +6,7 @@ for (let i = 0; i < bookingButtons.length; i++) {
     if (this.value === "Booking") {
       this.value = "Booked";
     } else {
-      this.value = "Booking"; // optional toggle
+      this.value = "Booking"; 
     }
   });
 }
